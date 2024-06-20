9297a165eadff9a6e48648e8e0c99266e47c7406
Debesh-Acharya <debeshacharya30@gmail.com>
2024-06-20 12:24:39 +0530

    TinyMce API integration

--- 12MegaBlog/src/components/RTE.jsx
+++ 12MegaBlog/src/components/RTE.jsx
@@ -12,6 +12,7 @@ export default function RTE({name,control,label,defaultValue=""}) {
         control={control}
         render={({field:{onChange}})=>(
             <Editor
+            apiKey='y1vnct9881zgaee6yls53rawz6tjfywb2816edidpzb1klkh'
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
